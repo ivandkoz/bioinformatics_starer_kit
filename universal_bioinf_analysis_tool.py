4cfa0b2c0306c14de3d2b73a2565217db38bafeb
Ivan Kozin <63678919+ivandkoz@users.noreply.github.com>
2023-10-08 15:32:46 +0300

    Create universal_bioinf_analysis_tool.py
    Add import from files and add class Analyze with three methods

---
+++ universal_bioinf_analysis_tool.py
@@ -0,0 +1,18 @@
+from bioinf_tools.dna_rna_analisys_tools import dna_rna_analysis
+from bioinf_tools.fasta_analisys_tool import fasta_analysis
+from bioinf_tools.protein_analysis_tool import protein_analysis
+
+
+class Analyze:
+"""
+This class includes three methods. 
+Each allows you to work with its own data type, similar to the name of the method"""
+
+    def fasta(*task):
+        return fasta_analysis(*task)
+
+    def protein(*task):
+        return protein_analysis(*task)
+
+    def dna_rna(*task):
+        return dna_rna_analysis(*task)
